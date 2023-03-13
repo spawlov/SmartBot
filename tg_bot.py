736d03542afc193d523053a8a3961b455c19a313
spawlov <spawlov@gmail.com>
2023-03-13 18:52:53 +0700

    Change telegram-bot library

--- tg_bot.py
+++ tg_bot.py
@@ -1,21 +1,36 @@
 import os
 
-import telebot
-
 from dotenv import load_dotenv, find_dotenv
+from telegram import Update, ForceReply
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, \
+    CallbackContext
+
+
+def start(update: Update, context: CallbackContext) -> None:
+    user = update.effective_user
+    update.message.reply_markdown_v2(
+        fr'Здравствуйте, {user.mention_markdown_v2()}\!',
+        reply_markup=ForceReply(selective=True),
+    )
+
+
+def echo(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(update.message.text)
+
+
+def main() -> None:
+    load_dotenv(find_dotenv())
+    tg_bot = Updater(os.getenv('TG_BOT_TOKEN'))
+
+    dispatcher = tg_bot.dispatcher
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(
+        MessageHandler(Filters.text & ~Filters.command, echo)
+    )
+
+    tg_bot.start_polling()
+    tg_bot.idle()
+
 
 if __name__ == '__main__':
-    load_dotenv(find_dotenv())
-    tg_bot = telebot.TeleBot(os.getenv('TG_BOT_TOKEN'))
-
-
-    @tg_bot.message_handler(commands=['start'])
-    def reply_start(message: telebot.types.Message):
-        tg_bot.send_message(message.chat.id, 'Здравствуйте')
-
-
-    @tg_bot.message_handler(content_types=['text'])
-    def echo(message: telebot.types.Message):
-        tg_bot.send_message(message.chat.id, message.text)
-
-    tg_bot.polling(none_stop=True)
+    main()
