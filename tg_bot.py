070e77f68e85797a8054331f83ae2b1bf97b38a1
spawlov <spawlov@gmail.com>
2023-04-09 02:41:26 +0700

    setup DialogFlow

--- tg_bot.py
+++ tg_bot.py
@@ -1,6 +1,8 @@
 import os
+import uuid
 
 from dotenv import load_dotenv, find_dotenv
+from google.cloud import dialogflow
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, \
     CallbackContext
@@ -8,14 +10,25 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, \
 
 def start(update: Update, context: CallbackContext) -> None:
     user = update.effective_user
-    update.message.reply_markdown_v2(
-        fr'Здравствуйте, {user.mention_markdown_v2()}\!',
-        reply_markup=ForceReply(selective=True),
+    update.message.reply_text(
+        f'Здравствуйте, {user.first_name} {user.last_name}!'
     )
 
 
 def echo(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(update.message.text)
+    project_id = os.getenv('PROJECT_ID')
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, str(uuid.uuid4()))
+    text_input = dialogflow.TextInput(
+        text=update.message.text,
+        language_code='ru-RU'
+    )
+    query_input = dialogflow.QueryInput(text=text_input)
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+    answer = response.query_result.fulfillment_text
+    update.message.reply_text(answer)
 
 
 def main() -> None:
