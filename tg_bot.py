088c132704ff045caac9cc350ebc90518e359b22
spawlov <spawlov@gmail.com>
2023-04-10 12:07:33 +0700

    added bot-logger

--- tg_bot.py
+++ tg_bot.py
@@ -1,8 +1,9 @@
-
 import os
 
 import requests
 from dotenv import load_dotenv, find_dotenv
+from loguru import logger
+from notifiers.logging import NotificationHandler
 from telegram import Update
 from telegram.ext import (
     Updater,
@@ -13,6 +14,8 @@ from telegram.ext import (
 )
 
 from extensions import detect_intent_texts, get_intents_list, create_intent
+
+logger.remove()
 
 
 def start(update: Update, context: CallbackContext) -> None:
@@ -31,28 +34,41 @@ def echo(update: Update, context: CallbackContext) -> None:
 def main() -> None:
     load_dotenv(find_dotenv())
     tg_bot = Updater(os.getenv('TG_BOT_TOKEN'))
+    tg_handler_params = {
+        'token': os.getenv('LOGGER_BOT_TOKEN'),
+        'chat_id': int(os.getenv('ALLOWED_CHAT_ID'))
+    }
+    tg_handler = NotificationHandler('telegram', defaults=tg_handler_params)
+    logger.add(tg_handler, level='INFO')
+
     project_id = os.getenv('PROJECT_ID')
-
     intents_list = get_intents_list(project_id)
-    questions = requests.get(os.getenv('QUESTIONS_URL')).json()
-    for display_name, items in questions.items():
-        if display_name not in intents_list:
-            result = create_intent(
-                project_id,
-                display_name,
-                items['questions'],
-                items['answer']
-            )
-            # print(f'Intent created: {result}')
-
-    dispatcher = tg_bot.dispatcher
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(
-        MessageHandler(Filters.text & ~Filters.command, echo)
-    )
-
-    tg_bot.start_polling()
-    tg_bot.idle()
+    try:
+        questions = requests.get(os.getenv('QUESTIONS_URL')).json()
+    except requests.exceptions.ReadTimeout as e:
+        logger.error(e)
+    except requests.exceptions.ConnectionError as e:
+        logger.error(e)
+    except requests.exceptions.HTTPError as e:
+        logger.error(e)
+    else:
+        for display_name, items in questions.items():
+            if display_name not in intents_list:
+                result = create_intent(
+                    project_id,
+                    display_name,
+                    items['questions'],
+                    items['answer']
+                )
+                logger.info(f'Intent created: {result}')
+    finally:
+        dispatcher = tg_bot.dispatcher
+        dispatcher.add_handler(CommandHandler("start", start))
+        dispatcher.add_handler(
+            MessageHandler(Filters.text & ~Filters.command, echo)
+        )
+        tg_bot.start_polling()
+        tg_bot.idle()
 
 
 if __name__ == '__main__':
