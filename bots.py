862553bc21a83aafd30637769bd53a2d26aa88f6
spawlov <spawlov@gmail.com>
2023-04-15 13:20:32 +0700

    added logger configurations

--- bots.py
+++ bots.py
@@ -9,6 +9,8 @@ from dialog_flow import intents_update
 from tg_bot import _tg_bot
 from vk_bot import _vk_bot
 
+logger.remove()
+
 
 def main():
     load_dotenv(find_dotenv())
@@ -17,6 +19,27 @@ def main():
         'chat_id': int(os.getenv('ALLOWED_CHAT_ID'))
     }
     tg_handler = NotificationHandler('telegram', defaults=tg_handler_params)
+    logger.add(
+        'debug.log',
+        format='{level}::{time}::{message}',
+        level='DEBUG',
+        rotation='0:00',
+        compression='zip',
+    )
+    logger.add(
+        'info.log',
+        format='{level}::{time}::{message}',
+        level='INFO',
+        rotation='0:00',
+        compression='zip',
+    )
+    logger.add(
+        'error.log',
+        format='{level}::{time}::{message}',
+        level='ERROR',
+        rotation='0:00',
+        compression='zip',
+    )
     logger.add(tg_handler, level='INFO')
     intents_update()
     Thread(target=_vk_bot).start()
